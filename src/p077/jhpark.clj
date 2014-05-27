a84ffb38dcda539364b5a11369fd27bff0ca55b3
JHPark <aquua@weebinar.com>
2014-05-27 14:02:31 +0900

    작업 중

--- src/p077/jhpark.clj
+++ src/p077/jhpark.clj
@@ -9,11 +9,13 @@
   (reduce str (sort string)))
 
 (defn to-map
+  ;;return {:eat "aet", :mate "aemt", :team "aemt", :mat "amt", :meat "aemt"}
   []
   (let [bb {}
         aa (for [a source]
              (assoc  bb (keyword  a) (sort-string a)))]
     (reduce conj aa)))
+
 
 
 (let [source2 (to-map)
@@ -25,6 +27,15 @@
 (def c  {:eat "aet", :mate "aemt", :team "aemt", :mat "amt", :meat "aemt"} )
 
 
-(filter #(= %  ~~ how to..) (distinct  (vals c) ))
+(doseq [val (distinct (vals (to-map)))]
+
+  )
+(map #(some #{%} (map val c))(distinct (vals c)))
+
+
+(keep #(some (set %) (map val c) )  (distinct (vals c)))
+
+(keep #(if (#(some #{%} (map val c)))  ) (distinct (vals c)))
+
 
 
