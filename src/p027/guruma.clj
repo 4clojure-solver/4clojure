06fd20d9d263680dc9720444cf72c120e40f9fe1
박상규 <psk810@gmail.com>
2014-06-19 14:13:48 +0900

    p027 solved by guruma.

---
+++ src/p027/guruma.clj
@@ -0,0 +1,20 @@
+(ns p025.guruma)
+
+(defn __ [v]
+  (= (seq v) (reverse v)))
+
+
+;; testcase-1
+(false? (__ '(1 2 3 4 5)))
+
+;; testcase-2
+(true? (__ "racecar"))
+
+;; testcase-3
+(true? (__ [:foo :bar :foo]))
+
+;; testcase-4
+(true? (__ '(1 1 3 3 1 1)))
+
+;; testcase-5
+(false? (__ '(:a :b :c)))
