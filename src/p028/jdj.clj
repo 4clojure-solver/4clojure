5dce3fcd4c9547e1585f6462dacf3acc3b0d868e
djey99 <djey9538@gmail.com>
2014-06-23 16:17:09 +0900

    (jdj) 28-29 solve

---
+++ src/p028/jdj.clj
@@ -0,0 +1,42 @@
+;; 4Clojure Question 28
+;;
+;; Write a function which flattens a sequence.
+;;
+;; Restrictions (please don't use these function(s)): flatten
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+
+(defn my-flatten [lst]
+  (let [s1 (take-while (comp not sequential?) lst)
+        s2 (drop-while (comp not sequential?) lst)]
+    (if (not (seq s2))
+      s1
+      (recur (concat s1 (first s2) (rest s2))))))
+
+
+
+
+(defn split-at-with [lst f]
+  (loop [s1 [] s2 lst]
+    (if (or (empty? s2) (f (first s2)))
+      [s1 s2]
+      (recur (conj s1 (first s2)) (rest s2)))))
+
+(defn my-flatten2 [lst]
+  (let [[s1 s2] (split-at-with lst sequential?)]
+    (if (empty? s2)
+      s1
+      (recur (concat s1 (first s2) (rest s2))))))
+
+
+
+
+
+(def __ my-flatten2)
+
+(= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+
+(= (__ ["a" ["b"] "c"]) '("a" "b" "c"))
+
+(= (__ '((((:a))))) '(:a))
---
+++ src/p029/jdj.clj
@@ -0,0 +1,21 @@
+;; 4Clojure Question 29
+;;
+;; Write a function which takes a string and returns a new string containing only the capital letters.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+
+
+(defn filter-UPPERCASE [s]
+  (apply str (filter #(Character/isUpperCase %) s)))
+
+
+
+
+(def __ filter-UPPERCASE)
+
+(= (__ "HeLlO, WoRlD!") "HLOWRD")
+
+(empty? (__ "nothing"))
+
+(= (__ "$#A(*&987Zf") "AZ")
