6c044ab0d71f4511b669f52ff6d5c871e5191d5d
jhpark <sonaclo@weebinar.com>
2014-06-25 13:54:38 +0900

    ㄱ

---
+++ src/p030/jhpark.clj
@@ -0,0 +1,18 @@
+(ns p030.jhpark)
+
+(def ori "Leeeeeerrroyyy")
+(def b [1 1 2 3 3 2 2 3])
+(def c [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2])
+
+
+(loop [data c acc []]
+  (if (empty? data)
+    acc
+    (do
+      (let [acc-list    (last acc)
+            data-first  (first data)]
+        (if (= acc-list data-first)
+          (recur (rest data) acc )
+          (recur (rest data)  (conj acc data-first)))
+        ) )))
+
