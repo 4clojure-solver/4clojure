24720d803c104cb423e34063695110f95f2140a7
manmyung <manmyung@gmail.com>
2014-07-14 17:10:15 +0900

    .

---
+++ src/p033/manmyung.clj
@@ -0,0 +1,13 @@
+(ns p033.manmyung)
+
+(defn replicate-seq
+  [lst i]
+  (mapcat (fn [t] (repeat i t)) lst))
+
+(def __ replicate-seq)
+
+(= (__ [1 2 3] 2) '(1 1 2 2 3 3))
+(= (__ [:a :b] 4) '(:a :a :a :a :b :b :b :b))
+(= (__ [4 5 6] 1) '(4 5 6))
+(= (__ [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
+(= (__ [44 33] 2) [44 44 33 33])
