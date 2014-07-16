622cce977bfe240171445db19d4dc0a9d9455cf4
manmyung <manmyung@gmail.com>
2014-07-16 14:09:02 +0900

    .

--- src/p034/manmyung.clj
+++ src/p034/manmyung.clj
@@ -3,6 +3,12 @@
 (defn __ [a b]
   (take (- b a) (iterate inc a)))
 
+;test
+(loop [i 2 result []]
+  (if (>= i 5)
+     result
+     (recur (inc i) (conj result i))))
+
 (= (__ 1 4) '(1 2 3))
 (= (__ -2 2) '(-2 -1 0 1))
 (= (__ 5 8) '(5 6 7))
